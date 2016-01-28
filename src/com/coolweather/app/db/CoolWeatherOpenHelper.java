09ef8dc914b8ef8e360e0e8b0bcc77ddfcd6f1c8
GaoRong <1543153960@qq.com>
2016-01-29 01:26:02 +0800

    新增数据库帮助类，及各表对应的实体类

---
+++ src/com/coolweather/app/db/CoolWeatherOpenHelper.java
@@ -0,0 +1,53 @@
+package com.coolweather.app.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
+
+	/**
+	 * Provice表建表语句
+	 */
+	public static final String CREATE_TABLE_PROVINCE = "create table province(" +
+			"id integer primary key autoincrement," +
+			"province_name text," +
+			"province_code text)";
+	/**
+	 * City表建表语句 
+	 */
+	public static final String CREATE_TABLE_CITY = "create table City(" +
+			"id integer primary key autoincrement," +
+			"city_name text," +
+			"city_code text," +
+			"province_id integer)";
+	/**
+	 * Country建表语句
+	 */
+	public static final String CERATE_TABLE_COUNTRY ="create table Country(" +
+			"id integer primary key autoincrement," +
+			"country_name text," +
+			"country_code text," +
+			"city_id integer)";
+	
+	
+	public CoolWeatherOpenHelper(Context context, String name,
+			CursorFactory factory, int version) {
+		super(context, name, factory, version);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL(CERATE_TABLE_COUNTRY);
+		db.execSQL(CREATE_TABLE_CITY);
+		db.execSQL(CREATE_TABLE_PROVINCE);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/coolweather/app/model/Province.java
@@ -0,0 +1,30 @@
+package com.coolweather.app.model;
+
+/**
+ * @author GaoR
+ *
+ */
+public class Province {
+	private int id;
+	private String provinceName;
+	private String proviceCode;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getProvinceName() {
+		return provinceName;
+	}
+	public void setProvinceName(String provinceName) {
+		this.provinceName = provinceName;
+	}
+	public String getProviceCode() {
+		return proviceCode;
+	}
+	public void setProviceCode(String proviceCode) {
+		this.proviceCode = proviceCode;
+	}
+
+}
